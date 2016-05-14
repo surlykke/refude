e5c4049d5bec32ddecb4b965d3370dd134d3319a
Christian Surlykke <christian@surlykke.dk>
2016-05-14 09:05:31 +0200

    Refude Do: Simplify and speedup icon cache

--- do/iconcache.js
+++ do/iconcache.js
@@ -6,45 +6,20 @@
  * Please refer to the LICENSE file for a copy of the license.
  */
 
-makeIconCache = function($http, resource) {
-    var iconRequestQueue = [];
-    fetcherIsWorking = false;
+makeIconCache = function ($http, resource) {
 
     var cache = {
         urls: {},
         requestIcon: function (iconName) {
-            iconRequestQueue.push(iconName);
-            if (!fetcherIsWorking) {
-                fetcher();
-            }
-        }
-    };
-
-    function fetcher() {
-        if (iconRequestQueue.length > 0) {
-            fetcherIsWorking = true;
-            var iconName = iconRequestQueue.shift();
-            if (iconName in cache.urls) {
-                // Move on to next request, then 
-                fetcher();
-            } 
-            else {
+            if (!(iconName in cache.urls)) {
+                cache[iconName] = null;
                 var url = resource + "?name=" + iconName;
-                console.log("fetch icon from url: ", url); 
                 $http.get(url, {responseType: 'blob'}).then(
                         function success(response) {
                             cache.urls[iconName] = window.URL.createObjectURL(response.data);
-                            fetcher();
-                        },
-                        function error(response) {
-                            cache.urls[iconName] = null;
-                            fetcher();
                         }
                 );
             }
-        }
-        else {
-            fetcherIsWorking = false;
         }
     };
 
