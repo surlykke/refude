1d01e4268cd33f7eb848a697fba8889c4a3ef6d7
Christian Surlykke <christian@surlykke.dk>
2016-05-14 14:47:43 +0200

    Refude Do: Fix 'scrollSelectedCommandIntoView'

--- do/do.js
+++ do/do.js
@@ -1,99 +1,105 @@
 /*
-* Copyright (c) 2015, 2016 Christian Surlykke
-*
-* This file is part of the refude project. 
-* It is distributed under the GPL v2 license.
-* Please refer to the LICENSE file for a copy of the license.
-*/
+ * Copyright (c) 2015, 2016 Christian Surlykke
+ *
+ * This file is part of the refude project. 
+ * It is distributed under the GPL v2 license.
+ * Please refer to the LICENSE file for a copy of the license.
+ */
 
 
 function doController($http, $scope, $window) {
     $scope.iconCache = makeIconCache($http, "http://localhost:7938/icons/icon");
-   
-    $scope.commands = []; 
-   
-    $scope.searchTerm = ""; 
-    
-    $scope.search = function() {
+
+    $scope.commands = [];
+
+    $scope.searchTerm = "";
+
+    $scope.search = function () {
         if ($scope.searchTerm === null) {
             $scope.searchTerm = "";
         }
-        
+
         if ($scope.searchTerm.trim() === "") {
             $scope.commands = [];
-        }
+        } 
         else {
             var url = "http://localhost:7938/desktopentries/commands?search=" + $scope.searchTerm;
-            $http.get(url).success(function(data) {
+            $http.get(url).success(function (data) {
                 $scope.commands = data.commands;
-              
+
                 // If the last selected command is no longer there, set selected command to first in list
                 if (selectedIndex() < 0 && $scope.commands.length > 0) {
                     selectedCommandId = $scope.commands[0].Id;
                 }
-                console.log("Set selectedCommand to " + selectedCommandId) ;
 
-                $scope.commands.forEach(function(command) { 
+                $scope.commands.forEach(function (command) {
                     $scope.iconCache.requestIcon(command.Icon);
                 });
             });
         }
     };
 
-    $scope.selected = function(command) {
+    $scope.selected = function (command) {
         return  selectedCommandId === command.Id;
     };
 
-    $scope.onKeyDown = function($event) {
+    $scope.onKeyDown = function ($event) {
         index = selectedIndex();
         if (index > -1) {
             if ($event.keyIdentifier === "Down" && index < $scope.commands.length - 1) {
                 selectedCommandId = $scope.commands[index + 1].Id;
-            }
-            else if ($event.keyIdentifier === "Up" && index > 0) {
+            } 
+	    else if ($event.keyIdentifier === "Up" && index > 0) {
                 selectedCommandId = $scope.commands[index - 1].Id;
-            }
+            } 
             else if ($event.keyIdentifier === "Enter" && selectedCommandId) {
                 $scope.selectCommand(selectedCommandId);
             }
-        } 
+        }
 
         scrollSelectedCommandIntoView();
     };
 
-    $scope.selectCommand = function(commandId) {
+    $scope.selectCommand = function (commandId) {
         console.log("Selected ", commandId);
         url = "http://localhost:7938/desktopentries/commands/" + commandId;
         $http.post(url);
-        $window.close(); 
+        $window.close();
     };
 
 
     selectedCommandId = null;
 
-        selectedIndex = function() {
-        return $scope.commands.findIndex(c =>  c.Id === selectedCommandId );
+    selectedIndex = function () {
+        return $scope.commands.findIndex(c => c.Id === selectedCommandId);
     };
 
-    scrollSelectedCommandIntoView = function() {
+    scrollSelectedCommandIntoView = function () {
         if (selectedCommandId) {
             contentDiv = document.getElementById("contentBox");
             commandDiv = document.getElementById(selectedCommandId);
             if (!(contentDiv && commandDiv)) {
                 return;
-            } 
-            contentDivRect = contentDiv.getBoundingClientRect(); 
-            commandDivRect = commandDiv.getBoundingClientRect();
-            if (commandDivRect.top < contentDivRect.top) {
-                contentDiv.scrollTop = contentDiv.scrollTop - (contentDivRect.top - commandDivRect.top + 15);
             }
-            else if (commandDivRect.bottom > contentDivRect.bottom) {
-                contentDiv.scrollTop = contentDiv.scrollTop + (commandDivRect.bottom - contentDivRect.bottom + 15);
+
+            contentTop = contentDiv.getBoundingClientRect().top;
+            commandTop = commandDiv.getBoundingClientRect().top;
+            contentBottom = contentDiv.getBoundingClientRect().bottom;
+            commandBottom = commandDiv.getBoundingClientRect().bottom;
+
+            delta = null;
+            if (commandTop < contentTop) {
+                // So command is (partly) above content view - we move the view upwards
+                delta = commandTop - contentTop - 15;
+            } else if (commandBottom > contentBottom) {
+                // So command is (partly) below content view - move the view downwards
+                delta = commandBottom - contentBottom + 15;
             }
-            //document.getElementById(commandId).scrollIntoView(false);
+            if (delta) {
+                contentDiv.scrollTop = contentDiv.scrollTop + delta;
+            }
         }
     };
-
 };
 
 
@@ -101,8 +107,8 @@ var doModule = angular.module('do', []);
 
 doModule.controller('doCtrl', ['$http', '$scope', '$window', doController]);
 
-doModule.config([ '$compileProvider', function ($compileProvider) {
-    $compileProvider.imgSrcSanitizationWhitelist(/^\s*((https?|ftp|file|blob|chrome-extension):|data:image\/)/);
-}]);
+doModule.config(['$compileProvider', function ($compileProvider) {
+        $compileProvider.imgSrcSanitizationWhitelist(/^\s*((https?|ftp|file|blob|chrome-extension):|data:image\/)/);
+    }]);
 
 
