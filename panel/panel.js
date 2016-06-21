758e4f148b10f3fdf1711ca82a5939fbda3e3dca
Christian Surlykke <christian@surlykke.dk>
2016-06-21 19:33:39 +0200

    Refude Do: Indicate when RefudePower service not available

--- panel/panel.js
+++ panel/panel.js
@@ -7,14 +7,30 @@
  */
 
 function panelController($scope, $interval, $http) {
-    $scope.charge = 50;
-    $scope.state = 3;
-    $scope.low = function() { return  $scope.state >= 2 ? 10 : 0; };
-    $scope.high = function() { return $scope.state >= 2 ? 30 : 0; };
-    $scope.stateSymbol = ['\u25CF', '\u002B', '\u2212', '\u25CB'];
-    $interval(function() {
+    $scope.stateSymbol = ['\u25CF', '\u002B', '\u2212', '\u25CB', '?'];
+
+    var updateBatteryInfo = function(event) {
+        if (event.type === "error") {
+            $scope.charge = 0;
+            $scope.state = 4;
+        }
+        else {
+            $http.get("http://localhost:7939/devices/battery_BAT0").then(function(response) {
+                $scope.charge = response.data.Percentage;
+                $scope.state = response.data.State;
+                $scope.low = function() { return  $scope.state >= 2 ? 10 : 0; };
+                $scope.high = function() { return $scope.state >= 2 ? 30 : 0; };
+            });
+        }
+    };
+
+    var evtSource = new EventSource("http://localhost:7939/notify");
+    evtSource.onmessage = evtSource.onerror = evtSource.onopen = updateBatteryInfo; 
+
+    $interval(function() { 
         $scope.time = new Date().toLocaleTimeString();
     });
+
 };
 
 var panelModule = angular.module('panel', []);
