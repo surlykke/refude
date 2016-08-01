f1102e40d4e5e3c70b3a3f4a8ecc6d58ff891877
Christian Surlykke <christian@surlykke.dk>
2016-08-01 10:42:30 +0200

    Change refude-panel to only show time
    Battery stuff will be in separat app

--- panel/panel.js
+++ panel/panel.js
@@ -6,36 +6,7 @@
  * Please refer to the LICENSE file for a copy of the license.
  */
 
-function panelController($scope, $timeout, $http) {
-    $scope.stateSymbol = ['\u25CF', '\u002B', '\u2212', '\u25CB', '?'];
-
-    var updateBatteryInfo = function(event) {
-        $http.get("http://localhost:8080/power/devices/battery_BAT0").then(function(response) {
-            $scope.charge = response.data.Percentage;
-            $scope.state = response.data.State;
-            $scope.low = function() { return  $scope.state >= 2 ? 10 : 0; };
-            $scope.high = function() { return $scope.state >= 2 ? 30 : 0; };
-        });
-    };
-
-    var evtSource = new EventSource("http://localhost:8080/power/notify");
-    evtSource.onerror = function(event) {
-        console.log("Error:", event);
-        $scope.charge = 0;
-        $scope.state = 4;
-    };
-
-
-    evtSource.onopen = function(event) {
-        console.log("open", event);
-        updateBatteryInfo();
-    }; 
-
-
-    evtSource.addEventListener("resource-updated", function(e) {
-        updateBatteryInfo();
-    });
-
+function panelController($scope, $timeout) {
     var setclock = function() {
         var now = new Date();
         $scope.time = now.toLocaleTimeString();
@@ -46,9 +17,6 @@ function panelController($scope, $timeout, $http) {
 };
 
 var panelModule = angular.module('panel', []);
-panelModule.controller('panelCtrl', ['$scope', '$timeout', '$http', panelController]);
-panelModule.config(['$compileProvider', function ($compileProvider) {
-    $compileProvider.imgSrcSanitizationWhitelist(/^\s*((https?|ftp|file|blob|chrome-extension):|data:image\/)/);
-}]);
+panelModule.controller('panelCtrl', ['$scope', '$timeout', panelController]);
 
 
