9c27b8c541e97f6e4d67886849b45db1aa5f207a
Christian Surlykke <christian@surlykke.dk>
2016-08-15 19:49:14 +0200

    Refude Battery: Add context menu
    - and do away with the annoying meter component in favour of divs

--- battery/battery.js
+++ battery/battery.js
@@ -5,18 +5,36 @@
  * It is distributed under the GPL v2 license.
  * Please refer to the LICENSE file for a copy of the license.
  */
-function batteryController($scope, $timeout, $http) {
+function batteryController($scope, $http) {
+    var state = 4;
 
-    $scope.stateStr = function() {
-        return "" + $scope.charge + "% " + ['\u25CF', '\u002B', '\u2212', '\u25CB', '?'][$scope.state];
+    $scope.charge = 0;
+    $scope.stateStr = "";
+    $scope.batteryclass = "battery-good";
+
+    var update = function() {
+        var stateChar = ["\u25CF", "\u002B", "\u2212", "\u25CB", "?"][state];
+        $scope.stateStr = "" + $scope.charge + "% (" + stateChar + ")";
+        
+        if (state < 2 || $scope.charge > 20) {
+            $scope.batteryclass = "battery-good";
+        }
+        else if ($scope.charge > 10) {
+            $scope.batteryclass = "battery-low";
+        }
+        else {
+            $scope.batteryclass = "battery-critical";
+        }
+        console.log("update done, $scope.stateStr: ", $scope.stateStr, 
+                    ", $scope.batteryclass: ", $scope.batteryclass);
     };
 
     var updateBatteryInfo = function(event) {
         $http.get("http://localhost:7938/power-service/devices/DisplayDevice").then(function(response) {
             $scope.charge = response.data.Percentage;
-            $scope.state =  response.data.State;
-            $scope.low = function() { return  $scope.state >= 2 ? 10 : 0; };
-            $scope.high = function() { return $scope.state >= 2 ? 30 : 0; };
+            state =  response.data.State;
+            console.log("charge now: ", $scope.charge, ", state:", state);
+            update(); 
         });
     };
 
@@ -24,7 +42,8 @@ function batteryController($scope, $timeout, $http) {
     evtSource.onerror = function(event) {
         console.log("Error:", event);
         $scope.charge = 0;
-        $scope.state = 4;
+        state = 4;
+        update();
     };
 
     evtSource.onopen = function(event) {
@@ -35,10 +54,15 @@ function batteryController($scope, $timeout, $http) {
     evtSource.addEventListener("resource-updated", function(e) {
         updateBatteryInfo();
     });
+
+    var openPowerSettings = function() {
+        $http.post("http://localhost:7938/desktop-service/applications/lxqt-leave.desktop");
+    };
+
+    chrome.contextMenus.create({"id": "PowerSettings", "title": "Power settings"});
+
+    chrome.contextMenus.onClicked.addListener(openPowerSettings);
 };
 
 var batteryModule = angular.module('battery', []);
-batteryModule.controller('batteryCtrl', ['$scope', '$timeout', '$http', batteryController]);
-batteryModule.config(['$compileProvider', function ($compileProvider) {
-    $compileProvider.imgSrcSanitizationWhitelist(/^\s*((https?|ftp|file|blob|chrome-extension):|data:image\/)/);
-}]);
+batteryModule.controller('batteryCtrl', ['$scope', '$http', batteryController]);
