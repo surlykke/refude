276ddbfd84227e45c780e071422660791cea3192
Christian Surlykke <christian@surlykke.dk>
2016-09-04 11:49:03 +0200

    Add panel as refude/electron app

--- install.sh
+++ install.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 sharedir="/usr/local/share/refude"
-filelist="do battery bin createwin.js main.js package.json"
+filelist="do battery panel bin createwin.js main.js package.json"
 cd $(realpath $(dirname $0))
 mkdir -p $sharedir
 for file in $filelist; do
 	cp -R $file $sharedir
 done
 
-chmod 755 $sharedir/bin/*.sh
+chmod 755 $sharedir/bin/*.sh 
+chmod 744 $sharedir/*/*.desktop
 
 cd /usr/local/bin
 for exe in $sharedir/bin/*.sh; do 
 	ln -sf $exe; 
+done
+
+cd /usr/local/share/applications
+for desktop in $sharedir/*/*.desktop; do 
+	ln -sf $desktop
 done
