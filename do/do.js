236cf41ddcabb2cbfa371e4f4a75b77439a18245
Christian Surlykke <christian@surlykke.dk>
2017-03-29 22:20:34 +0200

    Rewrote Do to use actions

--- do/do.js
+++ do/do.js
@@ -9,136 +9,163 @@
 function doController($q, $http, $scope, $window, $timeout) {
     const remote = require('electron').remote;
 
-    $scope.searchTerm = "";
-    $scope.actions = [];
+	$scope.searchTerm = ""
+	$scope.selectedUrl = undefined	
+	$scope.filteredUrls = []
+	$scope.action = actionUrl => $scope.resourceMap[actionUrl] || {}
+	$scope.resourceMap = {}
+
     $scope.version = 0;
+    $scope.$watch('version', () => { $scope.filterActions()});
+    $scope.$watch('searchTerm', () => { $scope.filterActions()});
 
-    let updateVersion = () => { $timeout(() => {$scope.version++;});};
-    let windowResourceFilter = res => !(res.States.includes("_NET_WM_STATE_ABOVE") || ["Refude Do", "refudeDo"].includes(res.Name));
+	let sources = []
 
-    let windowResourceActions = createResourceCollection($http,
-                                                         "http://localhost:7938/wm-service/windows",
-                                                         "http://localhost:7938/wm-service/notify",
-                                                         windowResourceFilter,
-                                                         updateVersion);
+	function Source(notifyUrl, actionListUrl) {
+		this.actionListUrl = actionListUrl
+		this.evtSource = new EventSource(notifyUrl)
+		
+		this.evtSource.onopen = () => {
+			$http.get(actionListUrl).then(response => {
+				$scope.resourceMap[actionListUrl] = response.data
+				response.data.forEach(actionPath => { getResource(actionPath)})
+			})
+		}
 
-    let applicationResourceFilter = res => true;
-    let applicationResourceActions = createResourceCollection($http,
-                                                              "http://localhost:7938/desktop-service/applications",
-                                                              "http://localhost:7938/desktop-service/notify",
-                                                              applicationResourceFilter,
-                                                              updateVersion);
+		this.evtSource.onerror = () => {
+			if ($scope.resourceMap[actionListUrl]) {
+				$scope.resourceMap[actionList].length = 0
+			}
+		}
+
+		this.evtSource.addEventListener("resource-added", (evt) => {
+			if (evt.data.startsWith("action")) getResource(evt.data)
+		})
+
+		this.evtSource.addEventListener("resource-updated", (evt) => {
+			if (evt.data.startsWith("action")) getResource(evt.data)
+		})
+
+		this.evtSource.addEventListener("resource-removed", (evt) => {
+			delete $scope.resourceMap[combineUrls(notifyUrl, evt.data)]
+			updateVersion()
+		})
+
+		let getResource = (resourcePath) => {
+			let resourceUrl = combineUrls(notifyUrl, resourcePath)
+			$http.get(resourceUrl).then(response => {
+				if (response.data.IconUrl) {
+					response.data.IconUrl = combineUrls(resourceUrl, response.data.IconUrl)
+				}
+				else if (response.data.IconName) {
+					response.data.IconUrl = iconServiceUrl(response.data.IconName, 32)
+				}
+				console.log(resourceUrl, response.data.IconUrl)
+				$scope.resourceMap[resourceUrl] = $scope.resourceMap[resourceUrl] || {}
+				Object.assign($scope.resourceMap[resourceUrl], response.data)
+				updateVersion()
+			})
+		}
+
+	}
+
+	$scope.filterActions = () => {
+		let term = $scope.searchTerm.toLowerCase().trim();
+		$scope.filteredUrls.length = 0;
+		sources.forEach(source => {
+			if ($scope.resourceMap[source.actionListUrl]) {
+				$scope.resourceMap[source.actionListUrl].forEach(actionPath => {
+					actionUrl = combineUrls(source.actionListUrl, actionPath)
+					act = $scope.resourceMap[actionUrl]
+					if (!act || ["Refude Do", "refudeDo"].includes(act.Name)) return
+				
+					if ((term === "" && act.X !== undefined) || (term !== "" && act.Name.toLowerCase().includes(term))) {
+						$scope.filteredUrls.push(actionUrl)
+					}
+				})
+			}
+		})
+
+		if ($scope.filteredUrls.indexOf($scope.selectedUrl) < 0) {
+			$scope.selectedUrl = $scope.filteredUrls[0]
+		}
+	}
 
 
+    //let updateVersion = () => { $timeout(() => {$scope.version++})}
+	let updateVersion = () => { $scope.version++}
     let history = {};
 
-    let filterActions = () => {
-        $scope.actions.length = 0;
-        let term = $scope.searchTerm.toLowerCase().trim();
-        let windowActionFilter = (action) => term.length === 0 || action.Name.toLowerCase().includes(term);
-        windowResourceActions.filter(windowActionFilter).forEach(action => $scope.actions.push(action));
-        let applicationActionFilter = (action) => term.length > 0 && action.Name.toLowerCase().includes(term);
-        applicationResourceActions.filter(applicationActionFilter)
-                                  .sort((act1, act2) => (history[act2.url] || 0) - (history[act1.url] || 0))
-                                  .forEach(action => $scope.actions.push(action));
-
-        if ($scope.actions.length > 0) {
-            let previous = $scope.actions[$scope.actions.length - 1];
-            $scope.actions.forEach((action) => {
-                action._previous = previous;
-                previous._next = action;
-                previous = action;
-            });
-        }
-
-        if ($scope.selectedAction) {
-            $scope.selectedAction = $scope.actions.find((action) => action.url === $scope.selectedAction.url);
-        }
-
-        if (!$scope.selectedAction && $scope.actions.length > 0) {
-            $scope.selectedAction = $scope.actions[0];
-        }
-    }
-
-
-    $scope.$watch('version', filterActions);
-    $scope.$watch('searchTerm', filterActions);
-
-
-    $scope.select = function(action) {
-		console.log("Selecting ", action)
-        $scope.selectedAction = action;
+    $scope.select = function(actionUrl) {
+        $scope.selectedUrl = actionUrl;
     };
 
-    $scope.selectAndExecute = function(action) {
-        $scope.select(action);
+    $scope.selectAndExecute = function(actionUrl) {
+        $scope.select(actionUrl);
         execute();
     };
 
     $scope.onKeyDown = function ($event) {
-		console.log("onKeyDown ", $event)
         if ($event.key === "Tab") {
-            action = keyActions[$event.shiftKey ? "ArrowUp" : "ArrowDown"];
+            keyAction = keyActions[$event.shiftKey ? "ArrowUp" : "ArrowDown"];
         }
         else {
-            action = keyActions[$event.key];
+            keyAction = keyActions[$event.key];
         }
-	
-		console.log("action ", action)
 
-        if (action) action();
+        if (keyAction) keyAction();
     };
 
-    $scope.actionClass  = function(action) {
+    $scope.actionClass  = (actionUrl) => {
         _class = ["line"];
-        if (action === $scope.selectedAction) {
+        if (actionUrl === $scope.selectedUrl) {
             _class.push("selected");
         }
-        if (action.X != undefined) {
+        if ($scope.action(actionUrl).X != undefined) {
             _class.push("shadow")
-            if (action.state && action.state.includes("_NET_WM_STATE_HIDDEN")) {
+            if ($scope.action(actionUrl).state && $scope.action(actionUrl).state.includes("_NET_WM_STATE_HIDDEN")) {
                 _class.push("dimmed");
             }
         }
         return _class;
-    };
+    }
 
-    $scope.style = function(action, index) {
+    $scope.style = function(actionUrl, index) {
         return {
-            "left" : "" + Math.round(scale*action.X) + "px",
-            "top" : "" + Math.round(scale*action.Y) + "px",
-            "width" : "" + Math.round(scale*action.W) + "px",
-            "height" : "" + Math.round(scale*action.H) + "px",
-            "z-index" : $scope.selectedAction === action ? 1000 : index,
-            "opacity" : $scope.selectedAction === action ? 0.7 : 0.3
+            "left" : "" + Math.round(scale*$scope.action(actionUrl).X) + "px",
+            "top" : "" + Math.round(scale*$scope.action(actionUrl).Y) + "px",
+            "width" : "" + Math.round(scale*$scope.action(actionUrl).W) + "px",
+            "height" : "" + Math.round(scale*$scope.action(actionUrl).H) + "px",
+            "z-index" : $scope.selectedUrl === actionUrl ? 1000 : index,
+            "opacity" : $scope.selectedUrl === actionUrl ? 0.7 : 0.3
         };
     };
 
     let next = () => {
-        if ($scope.selectedAction) {
-            $scope.selectedAction = $scope.selectedAction._next;
-        }
+		let i = $scope.filteredUrls.indexOf($scope.selectedUrl)
+		if (i >= 0) {
+			let len = $scope.filteredUrls.length
+			$scope.selectedUrl = $scope.filteredUrls[(i + 1) % len]
+		}	
         scrollSelectedCommandIntoView();
     };
 
     let previous = () => {
-        if ($scope.selectedAction) {
-            $scope.selectedAction = $scope.selectedAction._previous;
-        }
+       	let i = $scope.filteredUrls.indexOf($scope.selectedUrl)
+		if (i >= 0) {
+			let len = $scope.filteredUrls.length
+			$scope.selectedUrl = $scope.filteredUrls[(i + len - 1) % len]
+		} 
         scrollSelectedCommandIntoView();
     };
 
     let execute = function () {
-		console.log("Into execute, selectedAction: ", $scope.selectedAction)
-
-        let url = $scope.selectedAction.url;
-		console.log("execute, selectedActionUrl: ", $scope.selectedAction.url)
-        if (url) {
-			console.log("posting: ", url)
-            $http.post(url).then( response => {
+        if ($scope.selectedUrl) {
+			console.log("posting: ", $scope.selectedUrl)
+            $http.post($scope.selectedUrl).then( response => {
                 $scope.searchTerm = ""
                 remote.getCurrentWindow().hide()
-                history[url] = new Date().getTime();
+                history[$scope.selectedUrl] = new Date().getTime();
                 localStorage.setItem('history', JSON.stringify(history))
             }).then(error => {
                 console.log(error);
@@ -157,9 +184,9 @@ function doController($q, $http, $scope, $window, $timeout) {
     };
 
     let scrollSelectedCommandIntoView = function () {
-        if ($scope.selectedAction) {
+        if ($scope.selectedUrl) {
             let contentDiv = document.getElementById("contentBox");
-            let selectedDiv = document.getElementById($scope.selectedAction.url);
+            let selectedDiv = document.getElementById($scope.selectedUrl);
 
             if (contentDiv && selectedDiv) {
                 let contentRect = contentDiv.getBoundingClientRect();
@@ -194,18 +221,21 @@ function doController($q, $http, $scope, $window, $timeout) {
         scale = Math.min(width/displayGeometry.W, height/displayGeometry.H);
     };
 
-    $http.get("http://localhost:7938/wm-service/display").then(function(response) {
-        displayGeometry = response.data
-        calculateGeometry();
-        angular.element($window).bind('resize', calculateGeometry);
-    });
-
     try {
         history = JSON.parse(localStorage.getItem('history') || {});
     }
     catch (err) {
         history = {}
     }
+
+    $http.get("http://localhost:7938/wm-service/display").then(function(response) {
+        displayGeometry = response.data
+        calculateGeometry();
+        angular.element($window).bind('resize', calculateGeometry);
+    });
+
+	sources.push(new Source("http://localhost:7938/wm-service/notify", "http://localhost:7938/wm-service/actions"))
+	sources.push(new Source("http://localhost:7938/desktop-service/notify", "http://localhost:7938/desktop-service/actions"))
 
 };
 
