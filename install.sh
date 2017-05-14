9ce0aa40926095b7f2f54b0b1152fecf3c49a2db
Christian Surlykke <christian@surlykke.dk>
2017-05-14 13:25:14 +0200

    More proper install script

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 thisdir=$(dirname $(realpath $0))
 
-cd ~/bin
-angularapps="power/refudePower appconfig/refudeAppConfig connman/refudeConnman"
-
-for app in $angularapp; do
-	ln -sf $thisdir/$app
-done
+[[ -n "$PREFIX" ]] || PREFIX=$HOME/.local
+REFUDEDIR=${PREFIX}/share/refude
+BINDIR=${PREFIX}/bin
+mkdir -p ${REFUDEDIR}
 
 reactapps="panel/refudePanel do/refudeDo appchooser/refudeAppChooser"
 for app in $reactapps; do
-	appdir=`dirname $app`
+	appdir=$thisdir/`dirname $app`
 	echo "building $appdir"
-	(cd $thisdir/$appdir && gulp) && ln -sf $thisdir/dist/$app
+	cd $appdir
+	gulp || exit 1
+	cp -R $thisdir/dist/`dirname $app` ${PREFIX}/share/refude
+	ln -sf ${PREFIX}/share/refude/$app $BINDIR
 done
 
