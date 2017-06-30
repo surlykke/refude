30b70fd4df30f7ea9da6c1cd0ff9aca7e7df9214
Christian Surlykke <christian@surlykke.dk>
2017-06-30 17:40:07 +0200

    Panel, Notifications: Add dismiss button

--- panel/notifications/notifications.jsx
+++ panel/notifications/notifications.jsx
@@ -5,27 +5,54 @@ import {doHttp} from '../../common/utils'
 
 let Notification = (props) => {
 
-	let dismiss = () => {
+	let dismiss = (event) => {
+		console.log(event)
 		doHttp(props.item.url, "DELETE")
+		event.stopPropagation()
 	}
 
-	let action = (id) => {
-		doHttp(props.item.url + "?action=" + id)
+	let notificationClicked = (event) => {
+		console.log("notification clicked")
+		doHttp(props.item.url + "?action=default", "POST")
+		event.stopPropagation()
 	}
 
 	let {item} = props
-	console.log("Actions: ", item.Actions, ", keys: ", Object.keys(item.Actions))
+
+	let crossStyle = {
+		position: "absolute",
+		top: "3px",
+		right: "2px",
+		width: "15px",
+		height: "15px",
+	}
+
 	// dangerouslySetInnterHtml should be safe here - we rely on
 	// RefudeNotificationsService to sanitize notification body
 	return (
-		<div className="notification" onClick={() => action("default")}>
+		<div className="notification" style={{position: "relative"}} onClick={notificationClicked}>
 			<div className="notificationHeading">{item.Subject}</div>
-			<div
-				className="notificationBody"
-				dangerouslySetInnerHTML={{__html: item.Body}} />
-			{Object.keys(item.Actions).filter(k => k !== "default").map(k =>
-				<input type="submit" id={k} value={item.Actions[k]} onClick={() => {action(k)}}/>)
-			}
+			<div className="notificationBody" dangerouslySetInnerHTML={{__html: item.Body}} />
+
+			{Object.keys(item.Actions).filter(k => k !== "default").map(k => {
+				let buttonClicked = (event) => {
+					console.log("button", k, "clicked")
+					doHttp(props.item.url + "?action=" + k, "POST")
+					event.stopPropagation()
+				}
+
+				return 	<input type="submit" value={item.Actions[k]} onClick={buttonClicked}/>
+			})}
+
+			<div style={crossStyle} onClick={dismiss}>
+				<svg height="15px" width="15px" viewBox="0 0 100 100"
+					 strokeLinecap="round" stroke="grey" strokeWidth="10" >
+					<circle cx="50" cy="50" r="40" fill="none"/>
+					<line x1="32" y1="32" x2="68" y2="68"/>
+					<line x1="32" y1="68" x2="68" y2="32"/>
+				</svg>
+			</div>
+
 		</div>
 	 )
 }
