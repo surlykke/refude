3d06966b3c939d6cbb8a6fe4cb361e0635fcaea9
Christian Surlykke <christian@surlykke.dk>
2017-07-10 22:28:24 +0200

    DesktopService: change desktop POST to take query parameters rather than payload

--- appchooser/appchooser.jsx
+++ appchooser/appchooser.jsx
@@ -1,6 +1,6 @@
 // Copyright (c) 2015, 2016, 2017 Christian Surlykke
 //
-// This file is part of the refude project. 
+// This file is part of the refude project.
 // It is distributed under the GPL v2 license.
 // Please refer to the GPL2 file for a copy of the license.
 //
@@ -104,7 +104,7 @@ class AppChooser extends React.Component {
 				})
 			})
 		} else {
-			doHttp(app.url, "POST", {Arguments: [appArgument]}).then(resp => {
+			doHttp(app.url + "?arg=" + appArgument, "POST").then(resp => {
 				gui.App.quit()
 			})
 		}
