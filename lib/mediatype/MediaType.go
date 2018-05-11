4a949be27cc6686ff617a34b44d63848d9da69c0
Christian Surlykke <christian@surlykke.dk>
2018-05-11 12:24:51 +0200

    Add missing license headers

--- lib/mediatype/MediaType.go
+++ lib/mediatype/MediaType.go
@@ -1,3 +1,9 @@
+// Copyright (c) 2017,2018 Christian Surlykke
+//
+// This file is part of the RefudeServices project.
+// It is distributed under the GPL v2 license.
+// Please refer to the GPL2 file for a copy of the license.
+//
 package mediatype
 
 import (
