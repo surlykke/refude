d669d98bea2c6afedf1a6c839f8b773411dd290b
Christian Surlykke <christian@surlykke.dk>
2018-08-05 11:26:12 +0200

    Replace RefudeGET and RefudePOST with Refude
    Written in Go and capable of PATCH and DELETE

---
+++ Refude/Main.go
@@ -0,0 +1,68 @@
+// Copyright (c) 2017 Christian Surlykke
+//
+// This file is part of the RefudeServices project.
+// It is distributed under the GPL v2 license.
+// Please refer to the GPL2 file for a copy of the license.
+//
+package main
+
+import (
+	"context"
+	"net"
+	"net/http"
+	"github.com/surlykke/RefudeServices/lib"
+	"fmt"
+	"os"
+	"io/ioutil"
+	"bytes"
+	"strings"
+)
+
+func fail(msg string) {
+	fmt.Fprintln(os.Stderr, msg)
+	os.Exit(1)
+}
+
+func main() {
+	if len(os.Args) < 4 {
+		fail("Not enough arguments")
+	}
+	var method = os.Args[1]
+	var service = os.Args[2]
+	var path = os.Args[3]
+	var body = bytes.NewBuffer([]byte(strings.Join(os.Args[4:], " ")))
+
+	var client = http.Client{
+		Transport: &http.Transport{
+			DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
+				return net.Dial("unix", lib.RuntimeDir+"/org.refude." + service)
+			},
+		},
+	}
+	var url = "http://localhost" + path
+
+	if !lib.Contains([]string{"GET", "POST", "PATCH", "DELETE"}, method) {
+		fail("Method " + method + " not supported")
+	} else if lib.Contains([]string{"GET", "DELETE"}, method) && body.Len() > 0 {
+		panic("No body allowed")
+	} else if method == "PATCH" && body.Len() == 0 {
+		panic("Body mandatory")
+	}
+
+	if request, err := http.NewRequest(method, url, body); err != nil {
+		fail(err.Error())
+	} else if response, err := client.Do(request); err != nil {
+		fail(err.Error())
+	} else if body, err := ioutil.ReadAll(response.Body); err != nil {
+		fail(err.Error())
+	} else {
+		fmt.Fprint(os.Stderr, response.Proto, " ", response.Status, "\r\n")
+		for name, values := range response.Header {
+			for _,val := range values {
+				fmt.Fprint(os.Stderr, name, ":", val, "\r\n")
+			}
+		}
+		fmt.Fprint(os.Stderr, "\r\n")
+		fmt.Print(string(body))
+	}
+}
--- install.sh
+++ install.sh
@@ -8,14 +8,13 @@ BASH_COMPLETION_DIR=${XDG_DATA_HOME:-${HOME}/.local/share}/bash-completion/compl
 ZSH_COMPLETION_DIR=${XDG_DATA_HOME:-${HOME}/.local/share}/zsh/completions
 FISH_COMPLETION_DIR=${XDG_CONFIG_HOME:-${HOME}/.config}/fish/completions
 mkdir -p $BIN_DIR $BASH_COMPLETION_DIR $ZSH_COMPLETION_DIR $FISH_COMPLETION_DIR
-APPS="RefudeDesktopService RefudeIconService RefudePowerService RefudeWmService RefudeXdgOpen RefudeStatusNotifierService RefudeNotificationService RefudeProxy"
 
-
-for dir in $APPS; do
-	(cd $dir && echo "Building $dir" && go install) || exit 1
+for dir in Refude*; do
+    if [[ -d $dir ]]; then
+        (cd $dir && echo "Building $dir" && go install) || exit 1
+    fi
 done
 
-cp scripts/bin/* ${BIN_DIR}
 cp scripts/completions/bash/* ${BASH_COMPLETION_DIR}
 cp scripts/completions/zsh/* ${ZSH_COMPLETION_DIR}
 cp scripts/completions/fish/* ${FISH_COMPLETION_DIR}
