703165f73d8588a2aa4e110ecf614f116190639d
Christian Surlykke <christian@surlykke.dk>
2019-08-11 14:35:37 +0200

    Remove zsh completion
    Haven't used zsh for years and I don't remember how to do completion scripts

--- install.sh
+++ install.sh
@@ -4,10 +4,9 @@ cd `dirname $0`
 BIN_DIR=${HOME}/.local/bin
 GOPATH=${GOPATH:-${HOME}/go}
 GO_BIN_DIR=${GOPATH}/bin
-BASH_COMPLETION_DIR=${XDG_DATA_HOME:-${HOME}/.local/share}/bash-completion/completions
-ZSH_COMPLETION_DIR=${XDG_DATA_HOME:-${HOME}/.local/share}/zsh/completions
+BASH_COMPLETION_DIR=${XDG_DATA_HOME:-${HOME}/.local/share}/bash/completions
 FISH_COMPLETION_DIR=${XDG_CONFIG_HOME:-${HOME}/.config}/fish/completions
-mkdir -p $BIN_DIR $BASH_COMPLETION_DIR $ZSH_COMPLETION_DIR $FISH_COMPLETION_DIR
+mkdir -p $BIN_DIR $BASH_COMPLETION_DIR $FISH_COMPLETION_DIR
 
 for dir in refuc RefudeXdgOpen RefudeDesktopService; do
     if [[ -d $dir ]]; then
@@ -17,6 +16,5 @@ done
 
 cp scripts/bin/runRefude.sh $BIN_DIR
 cp scripts/completions/bash/* ${BASH_COMPLETION_DIR}
-cp scripts/completions/zsh/* ${ZSH_COMPLETION_DIR}
 cp scripts/completions/fish/* ${FISH_COMPLETION_DIR}
 ln -f $GO_BIN_DIR/RefudeXdgOpen $BIN_DIR/xdg-open
