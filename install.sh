d08fa88f44bf8c06f8240b6240925a855423b375
Christian Surlykke <christian@surlykke.dk>
2020-04-03 23:11:47 +0200

    Don't install RefudeXdgOpe. Not working

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ BASH_COMPLETION_DIR=${XDG_DATA_HOME:-${HOME}/.local/share}/bash/completions
 FISH_COMPLETION_DIR=${XDG_CONFIG_HOME:-${HOME}/.config}/fish/completions
 mkdir -p $BIN_DIR $BASH_COMPLETION_DIR $FISH_COMPLETION_DIR
 
-for dir in refuc RefudeXdgOpen RefudeDesktopService; do
+for dir in refuc RefudeDesktopService; do
     if [[ -d $dir ]]; then
         (cd $dir && echo "Building $dir" && go install) || exit 1
     fi
@@ -17,4 +17,3 @@ done
 cp scripts/bin/runRefude.sh $BIN_DIR
 cp scripts/completions/bash/* ${BASH_COMPLETION_DIR}
 cp scripts/completions/fish/* ${FISH_COMPLETION_DIR}
-ln -f $GO_BIN_DIR/RefudeXdgOpen $BIN_DIR/xdg-open
