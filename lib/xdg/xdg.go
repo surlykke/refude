dbfc173f4ac4737fec9ecc15e0635a451f2550e9
Christian Surlykke <christian@surlykke.dk>
2020-05-16 09:31:30 +0200

    xdg: clean all paths

--- lib/xdg/xdg.go
+++ lib/xdg/xdg.go
@@ -9,6 +9,7 @@ package xdg
 import (
 	"os"
 	"os/exec"
+	"path"
 	"strings"
 	"syscall"
 
@@ -35,14 +36,14 @@ var PicturesDir string
 var VideosDir string
 
 func init() {
-	Home = os.Getenv("HOME")
-	ConfigHome = notEmptyOr(os.Getenv("XDG_CONFIG_HOME"), Home+"/.config")
-	ConfigDirs = slice.Split(notEmptyOr(os.Getenv("XDG_CONFIG_DIRS"), "/etc/xdg"), ":")
-	CacheHome = notEmptyOr(os.Getenv("XDG_CACHE_HOME"), Home+"/.cache")
-	DataHome = notEmptyOr(os.Getenv("XDG_DATA_HOME"), Home+"/.local/share")
-	DataDirs = slice.Split(notEmptyOr(os.Getenv("XDG_DATA_DIRS"), "/usr/local/share:/usr/share"), ":")
+	Home = clean(os.Getenv("HOME"))
+	ConfigHome = clean(notEmptyOr(os.Getenv("XDG_CONFIG_HOME"), Home+"/.config"))
+	ConfigDirs = cleanS(slice.Split(notEmptyOr(os.Getenv("XDG_CONFIG_DIRS"), "/etc/xdg"), ":"))
+	CacheHome = clean(notEmptyOr(os.Getenv("XDG_CACHE_HOME"), Home+"/.cache"))
+	DataHome = clean(notEmptyOr(os.Getenv("XDG_DATA_HOME"), Home+"/.local/share"))
+	DataDirs = cleanS(slice.Split(notEmptyOr(os.Getenv("XDG_DATA_DIRS"), "/usr/local/share:/usr/share"), ":"))
 	DataDirs = slice.Remove(DataDirs, DataHome)
-	RuntimeDir = notEmptyOr(os.Getenv("XDG_RUNTIME_DIR"), "/tmp")
+	RuntimeDir = clean(notEmptyOr(os.Getenv("XDG_RUNTIME_DIR"), "/tmp"))
 	CurrentDesktop = slice.Split(notEmptyOr(os.Getenv("XDG_CURRENT_DESKTOP"), ""), ":")
 	Locale = notEmptyOr(os.Getenv("LANG"), "") // TODO Look at other env variables too
 	// Strip away encoding part (ie. '.UTF-8')
@@ -53,14 +54,14 @@ func init() {
 	// User dirs. Defaults taken from my /etc/xdg/user-dirs.defaults. We probably should re-read that file on startup,
 	// but given that so many apps use these, I find it unlikely that they will change. (The defaults, that is)
 	var userDirs, _ = readUserDirs(Home, ConfigHome)
-	DesktopDir = notEmptyOr(userDirs["XDG_DESKTOP_DIR"], Home+"/DESKTOP")
-	DownloadDir = notEmptyOr(userDirs["XDG_DOWNLOAD_DIR"], Home+"/DOWNLOAD")
-	TemplatesDir = notEmptyOr(userDirs["XDG_TEMPLATES_DIR"], Home+"/TEMPLATES")
-	PublicshareDir = notEmptyOr(userDirs["XDG_PUBLICSHARE_DIR"], Home+"/PUBLICSHARE")
-	DocumentsDir = notEmptyOr(userDirs["XDG_DOCUMENTS_DIR"], Home+"/DOCUMENTS")
-	MusicDir = notEmptyOr(userDirs["XDG_MUSIC_DIR"], Home+"/MUSIC")
-	PicturesDir = notEmptyOr(userDirs["XDG_PICTURES_DIR"], Home+"/PICTURES")
-	VideosDir = notEmptyOr(userDirs["XDG_VIDEOS_DIR"], Home+"/VIDEOS")
+	DesktopDir = clean(notEmptyOr(userDirs["XDG_DESKTOP_DIR"], Home+"/DESKTOP"))
+	DownloadDir = clean(notEmptyOr(userDirs["XDG_DOWNLOAD_DIR"], Home+"/DOWNLOAD"))
+	TemplatesDir = clean(notEmptyOr(userDirs["XDG_TEMPLATES_DIR"], Home+"/TEMPLATES"))
+	PublicshareDir = clean(notEmptyOr(userDirs["XDG_PUBLICSHARE_DIR"], Home+"/PUBLICSHARE"))
+	DocumentsDir = clean(notEmptyOr(userDirs["XDG_DOCUMENTS_DIR"], Home+"/DOCUMENTS"))
+	MusicDir = clean(notEmptyOr(userDirs["XDG_MUSIC_DIR"], Home+"/MUSIC"))
+	PicturesDir = clean(notEmptyOr(userDirs["XDG_PICTURES_DIR"], Home+"/PICTURES"))
+	VideosDir = clean(notEmptyOr(userDirs["XDG_VIDEOS_DIR"], Home+"/VIDEOS"))
 }
 
 func RunCmd(argv ...string) error {
@@ -88,4 +89,16 @@ func DirOrFileExists(dir string) bool {
 	} else {
 		return true
 	}
+}
+
+func clean(dir string) string {
+	return path.Clean(dir)
+}
+
+func cleanS(dirs []string) []string {
+	var cleaned = make([]string, 0, len(dirs))
+	for _, dir := range dirs {
+		cleaned = append(cleaned, dir)
+	}
+	return cleaned
 }
