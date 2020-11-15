8282b50750820ca88bc855a543f28e37ff7ef35b
Christian Surlykke <christian@surlykke.dk>
2020-11-15 21:27:45 +0100

    tweek minimize icon a bit

--- panel/minimizebutton.js
+++ panel/minimizebutton.js
@@ -16,8 +16,8 @@ export class MinimizeButton extends React.Component {
 	render = () =>
 		<div onClick={quit} className="clickable" title="Hide panel for 5 seconds">
 			<svg width="20" height="20" viewBox="0 0 100 100">
-				<path d="M 0 30 C 30 10, 60 0, 55 90" stroke="black" stroke-width="16" fill="none" />
-				<path d="M 20 60 L 55 90 L 90 60" stroke="black" stroke-width="18" fill="none" />
+				<path d="M 0 30 C 30 10, 60 0, 55 90" stroke="black" stroke-width="12" fill="none" />
+				<path d="M 20 60 L 55 90 L 90 60" stroke="black" stroke-width="14" fill="none" />
 			</svg>
 		</div>
 }
